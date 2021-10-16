24f0e79f74ff318b27cf8a098009246fda93f1f4
HiroIshida <spitfire.docomo@gmail.com>
2021-10-16 17:47:26 +0900

    Test predictor core

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -3,6 +3,29 @@ import torch
 from mimic.models import ImageAutoEncoder
 from mimic.models import LSTM
 from mimic.predictor import ImageLSTMPredictor
+from mimic.predictor import SimplePredictor
+from mimic.datatype import CommandDataChunk
+from mimic.dataset import AutoRegressiveDataset
+
+def test_predictor_core():
+    chunk = CommandDataChunk()
+    seq = np.random.randn(50, 7)
+    for i in range(10):
+        chunk.push_epoch(seq)
+    dataset = AutoRegressiveDataset.from_chunk(chunk)
+    seq = dataset[0][:29, :7]
+
+    lstm = LSTM(torch.device('cpu'), 7 + 1)
+    predictor = SimplePredictor(lstm)
+    for cmd in seq:
+        predictor.feed(cmd.detach().numpy())
+    assert torch.all(torch.stack(predictor.states) == seq)
+
+    cmd_pred = predictor.predict(n_horizon=1, with_feeds=False)
+
+    out = lstm(torch.unsqueeze(dataset[0][:30, :], dim=0))
+    cmd_pred_direct = out[0][-1, :-1].detach().numpy()
+    assert np.all(cmd_pred == cmd_pred_direct)
 
 def test_ImageLSTMPredictor():
     n_seq = 100
