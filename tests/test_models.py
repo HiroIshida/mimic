1af81bacb4a49cebd588873aa71bbba125d9aa32
HiroIshida <spitfire.docomo@gmail.com>
2021-10-18 03:46:44 +0900

    Put togather model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,24 @@
 import pytest
 import torch
 
+from mimic.models import ImageAutoEncoder
 from mimic.dataset import AutoRegressiveDataset
 from mimic.models import LSTM
-
 from test_datatypes import image_datachunk_with_encoder
 from test_datatypes import cmd_datachunk
+
+def test_image_auto_encoder():
+    n_batch = 100
+    n_channel = 3
+    for n_pixel in [28, 112, 224]:
+        sample = torch.randn(n_batch, n_channel, n_pixel, n_pixel)
+        ae = ImageAutoEncoder(torch.device('cpu'), 16, image_shape=(n_channel, n_pixel, n_pixel))
+
+        reconstructed = ae(sample)
+        assert reconstructed.shape == sample.shape
+        loss = ae.loss(sample)
+        assert len(list(loss.values())) == 1
+        assert float(loss['reconstruction'].item()) > 0.0 # check if positive scalar
 
 def test_lstm_with_image(image_datachunk_with_encoder): 
     dataset = AutoRegressiveDataset.from_chunk(image_datachunk_with_encoder)
@@ -15,4 +28,3 @@ def test_lstm_with_image(image_datachunk_with_encoder):
     loss = model.loss(sample)
     assert len(list(loss.values())) == 1
     assert float(loss['prediction'].item()) > 0.0 # check if positive scalar 
-
