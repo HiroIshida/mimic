e786da9280370500d80416375d73e83f49044407
HiroIshida <spitfire.docomo@gmail.com>
2021-10-19 18:30:05 +0900

    Strip and attach flag only if necessary

--- mimic/predictor.py
+++ mimic/predictor.py
@@ -15,17 +15,8 @@ from mimic.models import LSTM
 from mimic.models import DenseProp
 from abc import ABC, abstractmethod
 
-def attach_flag(vec: torch.Tensor) -> torch.Tensor: 
-    flag = torch.tensor([AutoRegressiveDataset.continue_flag])
-    return torch.cat((vec, flag))
-
-def strip_flag(vec: torch.Tensor) -> torch.Tensor: return vec[:-1]
-
-def force_continue_flag(vec: torch.Tensor) -> None: 
-    vec[-1] = AutoRegressiveDataset.continue_flag
-
-FBPropT = TypeVar('FBPropT', bound=Union[LSTM, DenseProp]) # Feedback propagator type
 StateT = TypeVar('StateT') # TODO maybe this is unncessarly
+FBPropT = TypeVar('FBPropT', bound=Union[LSTM, DenseProp]) # Feedback propagator type
 class AbstractPredictor(ABC, Generic[StateT, FBPropT]):
     propagator: FBPropT
     states: List[torch.Tensor]
@@ -38,15 +29,32 @@ class AbstractPredictor(ABC, Generic[StateT, FBPropT]):
         preds: List[torch.Tensor] = []
         for _ in range(n_horizon):
             states = torch.stack(feeds)
+            print(torch.unsqueeze(states, 0).shape)
             tmp = self.propagator(torch.unsqueeze(states, 0))
             out = torch.squeeze(tmp, dim=0)[-1].detach().clone()
-            force_continue_flag(out)
+            self.force_continue_flag_if_necessary(out)
             feeds.append(out)
             preds.append(out)
         return feeds if with_feeds else preds
 
+    def attach_flag_if_necessary(self, vec: torch.Tensor) -> torch.Tensor:
+        if isinstance(self.propagator, LSTM):
+            flag = torch.tensor([AutoRegressiveDataset.continue_flag])
+            return torch.cat((vec, flag))
+        return vec
+
+    def strip_flag_if_necessary(self, vec: torch.Tensor) -> torch.Tensor: 
+        if isinstance(self.propagator, LSTM):
+            return vec[:-1]
+        return vec
+
+    def force_continue_flag_if_necessary(self, vec: torch.Tensor) -> None: 
+        if isinstance(self.propagator, LSTM):
+            vec[-1] = AutoRegressiveDataset.continue_flag
+
     @abstractmethod
     def feed(self, state: StateT) -> None: ...
+
     @abstractmethod
     def predict(self, n_horizon: int, with_feeds: bool) -> List[StateT]: ...
 
@@ -55,12 +63,12 @@ class LSTMPredictor(AbstractPredictor[np.ndarray, FBPropT]):
     def feed(self, cmd: np.ndarray) -> None: 
         assert cmd.ndim == 1
         cmd_torch = torch.from_numpy(cmd).float()
-        cmd_with_flag = attach_flag(cmd_torch)
+        cmd_with_flag = self.attach_flag_if_necessary(cmd_torch)
         self.states.append(cmd_with_flag)
 
     def predict(self, n_horizon: int, with_feeds: bool=False) -> List[np.ndarray]:
         raw_preds = self._lstm_predict(n_horizon, with_feeds)
-        raw_preds_stripped = [strip_flag(pred) for pred in raw_preds]
+        raw_preds_stripped = [self.strip_flag_if_necessary(pred) for pred in raw_preds]
         preds_np = [pred.detach().numpy() for pred in raw_preds_stripped]
         return preds_np
 
@@ -76,12 +84,12 @@ class ImageLSTMPredictor(AbstractPredictor[np.ndarray, FBPropT]):
         img_torch = torchvision.transforms.ToTensor()(img).float()
         feature_ = self.auto_encoder.encoder(torch.unsqueeze(img_torch, 0))
         feature = torch.squeeze(feature_.detach().clone(), dim=0)
-        feature_with_flag = attach_flag(feature)
+        feature_with_flag = self.attach_flag_if_necessary(feature)
         self.states.append(feature_with_flag)
 
     def predict(self, n_horizon: int, with_feeds: bool=False) -> List[np.ndarray]:
         raw_preds = self._lstm_predict(n_horizon, with_feeds)
-        raw_preds_stripped = [strip_flag(pred) for pred in raw_preds]
+        raw_preds_stripped = [self.strip_flag_if_necessary(pred) for pred in raw_preds]
         image_preds = self.auto_encoder.decoder(torch.stack(raw_preds_stripped))
         lst = [np.array(torchvision.transforms.ToPILImage()(img)) for img in image_preds]
         return lst
@@ -102,7 +110,7 @@ class ImageCommandLSTMPredictor(AbstractPredictor[ImageCommandPair, FBPropT]):
         img_feature_ = self.auto_encoder.encoder(torch.unsqueeze(img_torch, 0))
         img_feature = torch.squeeze(img_feature_.detach().clone(), dim=0)
         feature = torch.cat((img_feature, cmd_torch))
-        feature_with_flag = attach_flag(feature)
+        feature_with_flag = self.attach_flag_if_necessary(feature)
         self.states.append(feature_with_flag)
 
     def predict(self, n_horizon: int, with_feeds: bool=False) -> List[ImageCommandPair]:
@@ -110,7 +118,7 @@ class ImageCommandLSTMPredictor(AbstractPredictor[ImageCommandPair, FBPropT]):
         n_cmd_feature = self.propagator.n_state - n_img_feature - 1
 
         raw_preds = self._lstm_predict(n_horizon, with_feeds)
-        raw_preds_stripped = [strip_flag(pred) for pred in raw_preds]
+        raw_preds_stripped = [self.strip_flag_if_necessary(pred) for pred in raw_preds]
 
         img_features, cmd_features\
                 = zip(*[(e[:n_img_feature], e[n_img_feature:]) for e in raw_preds_stripped])
